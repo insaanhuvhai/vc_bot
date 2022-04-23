b6e05e8e8fe08261a392d4a14fe4d8d63522064a
insaanhuvhai <104100121+insaanhuvhai@users.noreply.github.com>
2022-04-23 06:42:58 +0530

    Update helper.py

--- handlers/helper.py
+++ handlers/helper.py
@@ -9,21 +9,21 @@ from helpers.filters import command, other_filters2, other_filters
 @Client.on_message(command("help") & other_filters2)
 async def helper(ok, message: Message):
     await message.reply_text(
-        f"""💞 Hello! Following are the commands available for **{bn}** - __A Group Voice Chat Music Player__.
-The commands I currently support are:
-
-🔥 **Users Commands :**
-⚜️ /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
-⚜️ /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
-⚜️ /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
-⚜️ /repo - **[ DM Only ]** > __Gets the source code and YouTube Tutorial Video.__
+        f"""💞 Hello! Following are the commands available for **{bn}** - __I am fully lag free music player
+made with scratch__.
 
 
-🔰 **Admin & Sudo Users Commands :**
-⚜️ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
-⚜️ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
-⚜️ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
-⚜️ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
+ **Users Commands :**
+🔥 /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
+🔥 /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
+🔥 /ytplay - **[ Groups Only ]** > __Plays the song directly from YouTube Search.__
+
+
+ **Admin & Sudo Users Commands :**
+⚡ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
+⚡ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
+⚡ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
+⚡ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
 
 @Client.on_message(command("help") & other_filters)
 async def ghelp(_, message: Message):
